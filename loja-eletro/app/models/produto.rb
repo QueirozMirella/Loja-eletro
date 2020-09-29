27415d937747c217642e5ee7950d8c908059ffa1
Mirella Queiroz <queiroz.av@gmail.com>
2020-09-28 23:06:10 -0300

    Validações de produto

--- loja-eletro/app/models/produto.rb
+++ loja-eletro/app/models/produto.rb
@@ -1,2 +1,11 @@
 class Produto < ApplicationRecord
+    validates_presence_of :marca, message: 'Não pode ser deixado em branco'
+    validates_presence_of :codigo_produto, message: 'Não pode ser deixado em branco'
+    validates_presence_of :modelo, message: 'Não pode ser deixado em branco'
+    validates_presence_of :numero_serie, message: 'Não pode ser deixado em branco'
+    validates_presence_of :quantidade, message: 'Não pode ser deixado em branco'
+
+    validates :quantidade, numericality: { greater_than: 0, message: ' deve ser maior que 1' }
+    validates :numero_serie, numericality: { greater_than: 0, message: ' deve ser maior que 1' }
+    validates :codigo_produto, numericality: { greater_than: 0, message: ' deve ser maior que 1' }
 end
