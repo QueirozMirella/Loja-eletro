16c0714aa576f53ed1b096ec9187d100040ef2fb
Mirella Queiroz <queiroz.av@gmail.com>
2020-10-06 00:12:29 -0300

    validacao de venda

--- loja-eletro/app/models/venda.rb
+++ loja-eletro/app/models/venda.rb
@@ -1,2 +1,4 @@
 class Venda < ApplicationRecord
+    validates_presence_of :data_venda, message: 'Não pode ser deixado em branco'
+    validates_presence_of :codigo_venda, message: 'Não pode ser deixado em branco'
 end
