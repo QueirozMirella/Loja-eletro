15200a1f3d24f6e475e0ebafcc264d0460a688d6
Mirella Queiroz <queiroz.av@gmail.com>
2020-10-05 23:43:39 -0300

    validacoes de cliente

--- loja-eletro/app/models/cliente.rb
+++ loja-eletro/app/models/cliente.rb
@@ -1,2 +1,5 @@
 class Cliente < ApplicationRecord
+    validates_presence_of :codigo_cliente, message: 'Não pode ser deixado em branco'
+    validates_presence_of :nome, message: 'Não pode ser deixado em branco'
+    validates_presence_of :endereco, message: 'Não pode ser deixado em branco'
 end
