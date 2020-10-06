0c2abef372e68a25d5bdc798414e9353f71bb1a2
Mirella Queiroz <queiroz.av@gmail.com>
2020-10-06 00:07:47 -0300

    validacao de troca

--- loja-eletro/app/models/troca.rb
+++ loja-eletro/app/models/troca.rb
@@ -1,2 +1,4 @@
 class Troca < ApplicationRecord
+    validates_presence_of :defeito, message: 'Não pode ser deixado em branco'
+    validates_presence_of :data_troca, message: 'Não pode ser deixado em branco'
 end
