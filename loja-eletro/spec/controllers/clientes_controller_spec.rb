fcff82a7613e5779a7439b90854eed84589571c4
Mirella Queiroz <queiroz.av@gmail.com>
2020-10-06 23:53:03 -0300

    teste de cliente completo

--- loja-eletro/spec/controllers/clientes_controller_spec.rb
+++ loja-eletro/spec/controllers/clientes_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe ClientesController, :type => :controller do
     context "GET #index" do
@@ -32,6 +33,54 @@ RSpec.describe ClientesController, :type => :controller do
             get :show, params: { id: cliente.id }
             expect(assigns(:cliente)).to eq(cliente)
         end
+    end
 
+    context "GET #new" do
+        it "retornar uma nova pagina" do
+            get :new
+            expect(response).to have_http_status(200)
+            expect(response).to render_template(:new)
+        end
+
+        it "espera um novo cliente" do
+            get :new
+            expect(assigns(:cliente)).to be_a(Cliente)
+            expect(assigns(:cliente)).to be_a_new(Cliente)
+        end
+    end
+
+    context "GET #edit" do
+        let(:cliente) { create(:cliente) }
+        it "renderiza a pagina edit" do
+            get :edit, params: {id: cliente.id}
+            expect(response).to render_template(:edit)
+        end
+    end
+
+    context "PUT #update" do
+        let!(:cliente) { create(:cliente)}
+        it "atualizar um cliente" do
+            params = { nome: "Maria" }
+            put :update, params: { id: cliente.id, cliente: params}
+            cliente.reload
+            expect(cliente.nome).to eq(params[:nome])
+            expect(flash[:notice]).to eq("Cliente atualizado com sucesso.")
+            expect(response).to redirect_to(action: :show, id: assigns(:cliente).id)
+        end
+
+        it "nao atualiza um cliente" do
+            params = { nome: nil }
+            put :update, params: { id: cliente.id, cliente:params }
+            expect(response).to render_template(:edit)
+        end
+    end
+
+    context "DELETE #destroy" do
+        let!(:cliente) { create(:cliente) }
+        it "deleta cliente" do
+            delete :destroy, params: {id: cliente.id }
+            expect(flash[:notice]).to eq("Cliente excluído com sucesso.")
+            expect(response).to redirect_to(action: :index)
+        end
     end
 end
