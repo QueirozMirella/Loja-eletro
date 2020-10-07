7cff1b41b0d7e8fdc9bfbdd767fd53fdf7307510
Mirella Queiroz <queiroz.av@gmail.com>
2020-10-07 00:30:05 -0300

    commit final

--- loja-eletro/spec/factories/cliente.rb
+++ loja-eletro/spec/factories/cliente.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
     factory :cliente do
         codigo_cliente { '12345' }
         nome { 'Mirella Ribeiro Queiroz' }
-        endereco { 'Avenida Gilberto Cardoso, 117'}
+        endereco { 'Avenida Gilberto Cardoso, 117' }
     end
 end
--- loja-eletro/test/models/troca_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TrocaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
